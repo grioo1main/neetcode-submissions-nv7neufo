d35f86d97cd60776161d24ea76204e7d4b55371f
grioo1main <grioo1main@gmail.com>
2026-05-16 11:08:54 +0300

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.java
@@ -0,0 +1,40 @@
+class LRUCache {
+    private final int size;
+    private List<Integer> list = new LinkedList<>();
+    private HashMap<Integer, Integer> map = new HashMap<>();
+    private HashMap<Integer, Integer> mapCounter = new HashMap<>();
+
+    public LRUCache(int capacity) {
+        size = capacity;
+    }
+
+    public int get(int key) {
+        // 1. Если ключа нет в map, сразу возвращаем -1. Никаких update!
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        // 2. Если есть — обновляем историю и возвращаем значение
+        update(key);
+        return map.get(key);
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+        update(key);
+
+        while (map.size() > size) {
+            int l1 = list.removeLast();
+            if (mapCounter.get(l1) == 1) {
+                mapCounter.remove(l1);
+                map.remove(l1);
+            } else {
+                mapCounter.put(l1, mapCounter.get(l1) - 1);
+            }
+        }
+    }
+
+    public void update(int key) {
+        list.addFirst(key);
+        mapCounter.put(key, mapCounter.getOrDefault(key, 0) + 1);
+    }
+}
