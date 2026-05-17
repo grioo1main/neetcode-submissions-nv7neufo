1ce68e6271d059d5e93fb0aa980746c2da7a7f9e
grioo1main <grioo1main@gmail.com>
2026-05-17 10:23:58 +0300

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        int counter = 0;
+        boolean[][] bool = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            System.out.println("-+--");
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.println("--+-");
+                if (bool[i][j] == true) {
+                    continue;
+                }
+                System.out.println("----");
+                if (grid[i][j] == '1') {
+                    dfs(bool, i, j, grid);
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+    public void dfs(boolean[][] bool, int i, int j, char[][] grid) {
+        if (i >= grid.length || i < 0 || j >= grid[0].length || j < 0) {
+            return;
+        }
+
+        if (bool[i][j]) {
+            return;
+        }
+
+        if (grid[i][j] == '0') {
+            return;
+        }   
+        bool[i][j] = true;
+        dfs(bool, i + 1, j, grid);
+        dfs(bool, i - 1, j, grid);
+        dfs(bool, i, j + 1, grid);
+        dfs(bool, i, j - 1, grid);
+    }
+}
